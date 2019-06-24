bd21e85134976813f8b879ca66c570c6ad563c49
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2019-06-24 17:44:24 +0200

    Add another compiler unit test

--- test/CompileSpec.hs
+++ test/CompileSpec.hs
@@ -40,27 +40,45 @@ spec = after_ cleanup $ do
             it "compiles and runs a digit factorial calculator" $ do
                 testCompile factorial
                 testRun "5" `shouldReturn` "x" -- 'x' is ASCII for 120 (5 factorial)
-            it "compiles a brainfuck interpreter" $ do
+            it "compiles and tests a brainfuck interpreter" $ do
                 testCompile interpreter
+                let testString = factorial ++ "!" ++ "5"
+                testRun testString `shouldReturn` "x"
+            it "compiles and tests a brainfuck to C translator" $ do
+                testCompile bfToCTransaltor
                 hwCode <- testRun (helloWorld ++ "x")
                 writeFile "test-source.c" hwCode
                 callProcess "gcc" ["-w", "-o", "test.exe", "test-source.c"]
                 removeFile "test-source.c"
                 testRun "" `shouldReturn` "Hello World!\n"
+            
 
 -- Brainfuck programs
-
+-- |A program that prints "Hello World!\n"
 helloWorld :: String
 helloWorld = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
 
+-- |A cat program, repeats its input
 cat :: String
 cat = ",[.,]"
 
+-- |Reads a digit X and prints a character with ASCII code of X
 factorial :: String
 factorial = "++++++>,<[->--------<]>[[>>+>+<<<-]>>>-]+<[>[<[<+<+>>-]<[>+<-]>>-]<<<<]>."
 
+-- |Interprets BF code followed by '!' and its input [Source: http://www.hevanet.com/cristofd/brainfuck/]
 interpreter :: String
 interpreter = "\
+\>>>+[[-]>>[-]++>+>+++++++[<++++>>++<-]++>>+>+>+++++[>++>++++++<<-]+>>>,<++[[>[\
+\->>]<[>>]<<-]<[<]<+>>[>]>[<+>-[[<+>-]>]<[[[-]<]++<-[<+++++++++>[<->-]>>]>>]]<<\
+\]<]<[[<]>[[>]>>[>>]+[<<]<[<]<+>>-]>[>]+[->>]<<<<[[<<]<[<]+<<[+>+<<-[>-->+<<-[>\
+\+<[>>+<<-]]]>[<+>-]<]++>>-->[>]>>[>>]]<<[>>+<[[<]<]>[[<<]<[<]+[-<+>>-[<<+>++>-\
+\[<->[<<+>>-]]]<[>+<-]>]>[>]>]>[>>]>>]<<[>>+>>+>>]<<[->>>>>>>>]<<[>.>>>>>>>]<<[\
+\>->>>>>]<<[>,>>>]<<[>+>]<<[+<<]<]"
+
+-- |Translates input BF code to C code [Source: http://www.hevanet.com/cristofd/brainfuck/]
+bfToCTransaltor :: String
+bfToCTransaltor = "\
 \>+++++[>+++++++<-]>.<<++[>+++++[>+++++++<-]<-]>>.+++++.<++[>-----<-]>-.<++\
 \[>++++<-]>+.<++[>++++<-]>+.[>+>+>+<<<-]>>>[<<<+>>>-]<<<<<++[>+++[>---<-]<-\
 \]>>+.+.<+++++++[>----------<-]>+.<++++[>+++++++<-]>.>.-------.-----.<<++[>\
